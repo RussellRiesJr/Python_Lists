6c6dd5dd434794e876d2a3d52ee329908ff552f6
Russell Ries <riesru@gmail.com>
2016-07-08 11:16:08 -0500

    add extend

--- planets.py
+++ planets.py
@@ -5,7 +5,13 @@ def add_planets():
   planet_list.append("Saturn")
   print(planet_list)
 
+def more_planets():
+  planet_list.extend(["Uranus", "Neptune"])
+  print(planet_list)
 
+def call_all():
+  add_planets()
+  more_planets()
 
 import code
 code.interact(local=locals())
