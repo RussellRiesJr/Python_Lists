ab5f47ea8f0987a501ad86213f639c63687e03ae
Russell Ries <riesru@gmail.com>
2016-07-08 11:12:37 -0500

    add append list

---
+++ planets.py
@@ -0,0 +1,11 @@
+planet_list = ["Mercury", "Mars"]
+
+def add_planets():
+  planet_list.append("Jupiter")
+  planet_list.append("Saturn")
+  print(planet_list)
+
+
+
+import code
+code.interact(local=locals())
