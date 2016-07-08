92ae6a71a47650c02a9602de2ae09d83447d8dd2
Russell Ries <riesru@gmail.com>
2016-07-08 11:19:01 -0500

    add insert function

--- planets.py
+++ planets.py
@@ -9,9 +9,15 @@ def more_planets():
   planet_list.extend(["Uranus", "Neptune"])
   print(planet_list)
 
+def update_planets():
+  planet_list.insert(1, "Venus")
+  planet_list.insert(2, "Earth")
+  print(planet_list)
+
 def call_all():
   add_planets()
   more_planets()
+  update_planets()
 
 import code
 code.interact(local=locals())
