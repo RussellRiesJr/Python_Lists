84428476d505eeeb9486a8055b9ba7af12e6a605
Russell Ries <riesru@gmail.com>
2016-07-11 12:10:46 -0500

    finish random squared exercise

--- random_squared.py
+++ random_squared.py
@@ -1,7 +1,12 @@
 import random
 
-random_numbers =[random.sample(range(50), 20)]
+random_numbers = random.sample(range(50), 20)
 print(random_numbers)
+
+def squared(random_numbers):
+  return [num ** 2 for num in random_numbers]
+
+print(squared(random_numbers))
 
 import code
 code.interact(local=locals())
